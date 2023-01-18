d630f855fb5cc824e3550f9e241e034fb1b4ec9a
maciejthelackey <118081475+maciejthelackey@users.noreply.github.com>
2023-01-18 13:25:52 -0500

    added_cross_origin_header

--- src/main/java/com/maciej/wsbdataapi/controller/WsbController.java
+++ src/main/java/com/maciej/wsbdataapi/controller/WsbController.java
@@ -18,24 +18,27 @@ public class WsbController {
     public String health(@RequestParam String name){
         return "UP";
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/wsbData")
     public List<WsbResponse> callWsbData(@RequestParam String date){
         return wsbService.callWsbData(date);
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/wsbData/specific")
     public List<WsbResponse> callWsbDataTicker(@RequestParam List<String> tickers, @RequestParam String date){
         return wsbService.callWsbDataticker(tickers,date);
     }
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/wsbData/score")
     public List<WsbResponse> callSentimentScore(@RequestParam String date){
         return wsbService.callSentimentScore(date);
     }
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/wsbData/negative")
     public List<WsbResponse> callNegitiveScore(@RequestParam String date){
         return wsbService.callNegitiveScore(date);
     }
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("wsbData/negativeComments")
     public List<WsbResponse> callNegativeWithComments(@RequestParam String date){
         return wsbService.callNegativeWithComments(date);
